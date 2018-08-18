7401ec29ebe4df80a19cc4c57f175092ee1d5989
sho.endo <s.endo.8951@gmail.com>
2018-08-18 17:03:10 +0000

    signup and login at the same time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "ユーザーを登録しました"
+      session[:user_id] = @user.id
       redirect_to @user
     else
       flash.now[:danger] = "ユーザーの登録に失敗しました"
